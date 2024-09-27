27eda417cb6526b67915cc03a8f42083902b072b
Philipp Stracker <info@philippstracker.com>
2024-09-27 15:56:13 +0200

    🔨 Build theme bundles with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    entry: {
+        main: './src/index.js',
+        'theme/claude': './src/theme/claude/theme.js',
+        'theme/purple': './src/theme/purple/theme.js'
     },
-    compress: true,
-    port: 9000,
-  },
-  mode: 'development',
-  watch: true,
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: 'raw-loader',
+                include: path.resolve(__dirname, 'src/theme')
+            }
+        ]
+    },
+    plugins: [
+        new RemoveEmptyScriptsPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep']
+        })
+    ]
 };
