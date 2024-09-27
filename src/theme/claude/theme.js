b958e93cf74ac74597b687763347b3f4b650f7be
Philipp Stracker <info@philippstracker.com>
2024-09-27 15:56:41 +0200

    🏗️ Prepare the theme js bundles

---
+++ src/theme/claude/theme.js
@@ -0,0 +1,9 @@
+import './style.scss'
+
+import svgNewChat from './new-chat.svg'
+import svgOpenSidebar from './open-sidebar.svg'
+
+const icons = new Map();
+
+icons.set('new-chat', svgNewChat);
+icons.set('open-sidebar', svgOpenSidebar);
