486c6c94ddd1f2e101c32b854137297e81bb0bac
Philipp Stracker <info@philippstracker.com>
2024-10-04 20:49:16 +0200

    🏗️ Change build system output
    Themes are packaged into a <theme>.js and <theme>.css file, instead of placing the files into a <theme>-subfolder

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,43 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        'theme/claude': './src/theme/claude/theme.js',
-        'theme/purple': './src/theme/purple/theme.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                use: 'raw-loader',
-                include: path.resolve(__dirname, 'src/theme')
-            }
-        ]
-    },
-    plugins: [
-        new RemoveEmptyScriptsPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep']
-        })
-    ]
+	mode: 'development',
+	entry: {
+		main: './src/index.js',
+		'theme/claude': './src/theme/claude/theme.js',
+		'theme/purple': './src/theme/purple/theme.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		allowedHosts: 'all',
+		compress: true,
+		port: 9000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+				],
+			}, {
+				test: /\.svg$/,
+				use: 'raw-loader',
+				include: path.resolve(__dirname, 'src/theme'),
+			},
+		],
+	},
+	plugins: [
+		new RemoveEmptyScriptsPlugin(), new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
 };
